233e26f0e17ab4cae00066f2869afc3da8526b47
colinmarchbanks <marchbanks.colin@gmail.com>
2020-03-27 16:30:01 -0500

    finished the breaking the record problem

---
+++ src/breakingTheRecords.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the breakingRecords function below.
+    static int[] breakingRecords(int[] scores) {
+        int breaksLow = 0;
+        int breaksHigh = 0;
+        int currentLow = scores[0];
+        int currentHigh = scores[0];
+        for(int x = 1; x<scores.length;x++){
+            if(currentLow > scores[x]){
+                currentLow = scores[x];
+                breaksLow++;
+            }
+            if(currentHigh < scores[x]){
+                currentHigh = scores[x];
+                breaksHigh++;
+            }
+        }
+        int[] bucket = {breaksHigh,breaksLow};
+        return bucket;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] scores = new int[n];
+
+        String[] scoresItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int scoresItem = Integer.parseInt(scoresItems[i]);
+            scores[i] = scoresItem;
+        }
+
+        int[] result = breakingRecords(scores);
+
+        for (int i = 0; i < result.length; i++) {
+            bufferedWriter.write(String.valueOf(result[i]));
+
+            if (i != result.length - 1) {
+                bufferedWriter.write(" ");
+            }
+        }
+
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
