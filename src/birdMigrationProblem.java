def63fec2f4e5bafc6b48dbf8b9714e81f50209a
colinmarchbanks <marchbanks.colin@gmail.com>
2020-03-29 22:40:59 -0500

    Finsihed the bird migration problem

---
+++ src/birdMigrationProblem.java
@@ -0,0 +1,57 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+public class Solution {
+
+    // Complete the migratoryBirds function below.
+    static int migratoryBirds(List<Integer> arr) {
+        int birdType = 0;
+        int birdNum = 0;
+        int newNum = 0;
+        int total = 1;
+        Collections.sort(arr);
+        for(int x = 0; x < arr.size();x++){
+            if(arr.get(x) != newNum){
+                total = 1;
+                newNum = arr.get(x);
+            }
+            if(arr.get(x) == newNum){
+                total++;
+                if(total > birdNum){
+                    birdType = newNum;
+                    birdNum = total;
+                }
+            }
+        }
+        return birdType;
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int arrCount = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> arr = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                .map(Integer::parseInt)
+                .collect(toList());
+
+        int result = migratoryBirds(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
